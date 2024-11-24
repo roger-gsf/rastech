95a5bc63afe0a311a2a28a0db29e54430b788190
04374771000 <04374771000@senacrs.edu.br>
2024-11-24 01:29:25 -0300

    Adding database

---
+++ schema.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE rastech;
+USE rastech;
+
+CREATE TABLE usuarios (
+    usuario_id INT PRIMARY KEY AUTO_INCREMEN
+);
+
+CREATE TABLE itens (
+    item_id INT PRIMARY KEY AUTO_INCREMENT,
+    item_nome VARCHAR(255) NOT NULL,
+    item_empresa VARCHAR(255) NOT NULL, -- Opcional
+    item_status ENUM('Contaminado', 'EmEsterilizacao', 'Esterilizado', 'EmUso') NOT NULL,
+    item_validade_autoclave DATE NOT NULL,
+    item_lote_autoclave INT NOT NULL,
+    cb_id FOREIGN KEY codigos_de_barras(codigos_de_barras.cb_id),
+    caixa_id FOREIGN KEY REFERENCES caixas(caixa_id)
+);
+
+CREATE TABLE caixas (
+    caixa_id INT PRIMARY KEY AUTO_INCREMENT,
+    caixa_status ENUM('Contaminada', 'EmEsterilizacao', 'Esterilizada', 'EmUso') NOT NULL,
+    caixa_local ENUM('Estoque', 'CME', 'CentroCirurgico') NOT NULL,
+    caixa_sala  VARCHAR(255)
+);
+
+CREATE TABLE codigos_de_barras (
+    cb_id     INT PRIMARY KEY AUTO_INCREMENT,
+    cb_codigo VARCHAR(16) NOT NULL,
+);
