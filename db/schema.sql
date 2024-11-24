c090751c5f3a35e7daee6fec90a9fbd50c5169e2
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2024-11-24 04:54:32 -0300

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,29 +3,9 @@ USE rastech;
 
 CREATE TABLE usuarios (
     usuario_id INT PRIMARY KEY AUTO_INCREMENT,
-    usuario_nome VARCHAR(255) NOT NULL,
-    usuario_senha VARCHAR(255) NOT NULL,
-    usuario_position ENUM('Admin', 'Médico', 'Enfermeiro', 
-    'TécnicoDeEnfermagem', 'Instrumentador') NOT NULL
-);
-
-CREATE TABLE itens (
-    item_id INT PRIMARY KEY AUTO_INCREMENT,
-    item_nome VARCHAR(255) NOT NULL,
-    item_empresa VARCHAR(255) NOT NULL, -- Opcional
-    item_status ENUM('Contaminado', 'EmEsterilizacao', 'Esterilizado', 
-    'EmUso', 'Extraviado', 'Danificado') NOT NULL,
-    item_validade_autoclave DATE NOT NULL,
-    item_lote_autoclave INT NOT NULL,
-    cb_id FOREIGN KEY codigos_de_barras(codigos_de_barras.cb_id),
-    caixa_id FOREIGN KEY REFERENCES caixas(caixa_id)
-);
-
-CREATE TABLE caixas (
-    caixa_id INT PRIMARY KEY AUTO_INCREMENT,
-    caixa_status ENUM('Contaminada', 'EmEsterilizacao', 'Esterilizada', 'EmUso') NOT NULL,
-    caixa_local ENUM('Estoque', 'CME', 'CentroCirurgico') NOT NULL,
-    caixa_sala VARCHAR(255)
+    usuario_nome varchar(255) not null,
+    usuario_senha varchar(255) not null,
+    usuario_position ENUM('Admin', 'Médico', 'Enfermeiro', 'TécnicoDeEnfermagem', 'Instrumentador') not null
 );
 
 CREATE TABLE codigos_de_barras (
@@ -33,29 +13,58 @@ CREATE TABLE codigos_de_barras (
     cb_codigo VARCHAR(16) NOT NULL
 );
 
+CREATE TABLE caixas (
+    caixa_id INT PRIMARY KEY AUTO_INCREMENT,
+    caixa_status ENUM('Contaminada', 'EmEsterilizacao', 'Esterilizada', 'EmUso', 'ParaUso') NOT NULL,
+    caixa_local ENUM('Estoque', 'CME', 'CentroCirurgico') NOT NULL,
+    caixa_sala VARCHAR(255)
+);
+
+
+CREATE TABLE itens (
+    item_id INT PRIMARY KEY AUTO_INCREMENT,
+    item_nome VARCHAR(255) NOT NULL,
+    item_empresa VARCHAR(255) NOT NULL, -- Opcional
+    item_status ENUM('Contaminado', 'EmEsterilizacao', 'Esterilizado', 'EmUso', 'Extraviado', 'Danificado') NOT NULL,
+    item_validade_autoclave DATE NOT NULL,
+    item_lote_autoclave INT NOT NULL,
+    cb_id int NOT NULL,
+    FOREIGN KEY (cb_id) REFERENCES codigos_de_barras(cb_id),
+    caixa_id int not null,
+    FOREIGN KEY (caixa_id) REFERENCES caixas(caixa_id)
+);
+
+
+
 CREATE TABLE kits_prontos (
-    kit_id INT PRIMARY KEY AUTO_INCREMENT,
-    kit_procedimento_destinado VARCHAR(255) NOT NULL,
-    kit_descricao TEXT
+    kit_id int PRIMARY key AUTO_INCREMENT,
+    kit_procedimento_destinado varchar(255) not null,
+    kit_descricao text
 );
 
 CREATE TABLE kit_itens (
     kit_itens_id INT PRIMARY KEY AUTO_INCREMENT,
-    kit_id INT FOREIGN KEY kits_prontos(kit_id),
-    kit_id INT FOREIGN KEY itens(item_id)
+    kit_id int,
+    foreign key (kit_id) references kits_prontos(kit_id),
+    item_id int,
+    foreign key (item_id) references itens(item_id)
 );
 
 CREATE TABLE kit_pedidos_historico (
     pedido_id INT PRIMARY KEY AUTO_INCREMENT,
-    sala_numero INT NOT NULL,
-    medico_id INT FOREIGN KEY usuarios(usuario_id) NOT NULL,
-    caixa_id INT FOREIGN KEY caixas(caixa_id) NOT NULL,
+    sala_numero INT not null,
+    medico_id INT, 
+    FOREIGN KEY (medico_id) references usuarios(usuario_id),
+    caixa_id INT,
+    foreign key (caixa_id) references caixas(caixa_id),
     data_encomenda datetime,
     pedido_entregue boolean
 );
 
 CREATE TABLE adds_pedidos (
-    adicao_id INT PRIMARY KEY AUTO_INCREMENT,
-    pedido_id INT FOREIGN KEY kit_pedidos_historico(pedido_id),
-    item_id INT FOREIGN KEY itens(item_id)
+    adicao_id int PRIMARY key AUTO_INCREMENT,
+    pedido_id int,
+    foreign key (pedido_id) references kit_pedidos_historico(pedido_id),
+    item_id int,
+    foreign key (item_id) references itens(item_id)
 );
