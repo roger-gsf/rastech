c7498c005d33b0467fa1efe963b53015864107a4
Gustav-hdoliveira <162641711+Gustav-hdoliveira@users.noreply.github.com>
2024-11-24 06:27:23 -0300

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,14 @@ CREATE TABLE usuarios (
     usuario_position ENUM('Admin', 'Médico', 'Enfermeiro', 'TécnicoDeEnfermagem', 'Instrumentador') not null
 );
 
+CREATE TABLE caixas (
+    caixa_id INT PRIMARY KEY AUTO_INCREMENT,
+    caixa_status ENUM('Contaminada', 'EmEsterilizacao', 'Esterilizada', 'EmUso', 'ParaUso') NOT NULL,
+    caixa_local ENUM('Estoque', 'CME', 'CentroCirurgico') NOT NULL,
+    caixa_sala VARCHAR(255)
+);
+
+
 CREATE TABLE itens (
     item_id INT PRIMARY KEY AUTO_INCREMENT,
     item_nome VARCHAR(255) NOT NULL,
@@ -15,20 +23,10 @@ CREATE TABLE itens (
     item_status ENUM('Contaminado', 'EmEsterilizacao', 'Esterilizado', 'EmUso', 'Extraviado', 'Danificado') NOT NULL,
     item_validade_autoclave DATE NOT NULL,
     item_lote_autoclave INT NOT NULL,
-    cb_id FOREIGN KEY codigos_de_barras(codigos_de_barras.cb_id),
-    caixa_id FOREIGN KEY REFERENCES caixas(caixa_id)
-);
-
-CREATE TABLE caixas (
-    caixa_id INT PRIMARY KEY AUTO_INCREMENT,
-    caixa_status ENUM('Contaminada', 'EmEsterilizacao', 'Esterilizada', 'EmUso') NOT NULL,
-    caixa_local ENUM('Estoque', 'CME', 'CentroCirurgico') NOT NULL,
-    caixa_sala VARCHAR(255)
-);
-
-CREATE TABLE codigos_de_barras (
-    cb_id     INT PRIMARY KEY AUTO_INCREMENT,
-    cb_codigo VARCHAR(16) NOT NULL
+    tipo_id int,
+    foreign key (tipo_id) references itens_tipo(tipo_id)
+    caixa_id int not null,
+    FOREIGN KEY (caixa_id) REFERENCES caixas(caixa_id)
 );
 
 CREATE TABLE kits_prontos (
@@ -39,21 +37,24 @@ CREATE TABLE kits_prontos (
 
 CREATE TABLE kit_itens (
     kit_itens_id INT PRIMARY KEY AUTO_INCREMENT,
-    kit_id int foreign key kits_prontos(kit_id),
-    kit_id int foreign key itens(item_id)
+    kit_id int,
+    foreign key (kit_id) references kits_prontos(kit_id),
+    item_id int,
+    foreign key (item_id) references itens(item_id)
 );
+
+CREATE TABLE itens_tipo (
+    tipo_id int PRIMARY key AUTO_INCREMENT,
+    nome_tipo varchar(255) not null
+)
 
 CREATE TABLE kit_pedidos_historico (
     pedido_id INT PRIMARY KEY AUTO_INCREMENT,
     sala_numero INT not null,
-    medico_id INT FOREIGN KEY usuarios(usuario_id) not null,
-    caixa_id INT foreign key caixas(caixa_id) not null,
+    medico_id INT, 
+    FOREIGN KEY (medico_id) references usuarios(usuario_id),
+    caixa_id INT,
+    foreign key (caixa_id) references caixas(caixa_id),
     data_encomenda datetime,
     pedido_entregue boolean
-);
-
-CREATE TABLE adds_pedidos (
-    adicao_id int PRIMARY key AUTO_INCREMENT,
-    pedido_id int foreign key kit_pedidos_historico(pedido_id),
-    item_id int foreign key itens(item_id)
 );
